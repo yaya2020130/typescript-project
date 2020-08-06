bb9664acb0f255e393e0e3d7e2c16c95cd5ac7d7
yaya <anbesie.yg@gmail.com>
2020-08-05 17:52:04 -0700

    createed typescript folder

---
+++ quiz/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App"> ' Quiz'
+    </div>
+  );
+}
+
+export default App;
